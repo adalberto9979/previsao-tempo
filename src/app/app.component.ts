0d7b8f81a5ad3dbbd6982ce87189460d142f0521
Adalberto Nascimento <adalbertos.nascimento@gmail.com>
2021-04-25 22:50:50 -0300

    Entrega final.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,100 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { timeout } from 'rxjs/operators';
+import { Cidade } from './app.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'previsao-tempo';
+
+@Injectable()
+export class AppComponent implements OnInit {
+   cidades: Cidade[] = [];
+   constructor(private http: HttpClient) {
+   }
+
+   ngOnInit() {
+      this.doGet<Cidade[]>('SP').subscribe(response => {
+        this.cidades = response;
+      });
+   }
+
+   doGet<T>(estado: string): Observable<T> {
+      let apiToken = "84217b124d9b23d1acf3f0ac75bbe18d";
+      
+      let params = new HttpParams({fromObject: {
+         Headers: 'Access-Control-Allow-Origin, http://localhost:4200'
+       }});
+
+      return this.http.get<T>(`http://apiadvisor.climatempo.com.br/api/v1/locale/city?province=${estado}&token=${apiToken}/`, { params })
+      .pipe(timeout(5000));
+   }   
+
+  // 84217b124d9b23d1acf3f0ac75bbe18d token clima tempo
+  /*
+    /api/v1/locale/city?name=CityName&state=StateAbbr&country=BR&token=your-app-token
+    curl -i "http://apiadvisor.climatempo.com.br/api/v1/locale/city?name=São Paulo&state=SP&token=your-app-token"
+    curl -i "http://apiadvisor.climatempo.com.br/api/v1/locale/city?state=SP&name=são&token=your-app-token"
+    curl -i "http://apiadvisor.climatempo.com.br/api/v1/locale/city?province=SP&token=your-app-token"
+    curl -i "http://apiadvisor.climatempo.com.br/api/v1/locale/city?country=BR&token=your-app-token"    
+    [
+      {
+          id: 3477,
+          name: "São Paulo",
+          state: "SP",
+          country: "BR  "
+      },
+      ...
+    ]
+
+    /api/v1/climate/temperature/locale/:id?token=your-app-token
+    curl -v -L "http://apiadvisor.climatempo.com.br/api/v1/climate/temperature/locale/3477?token=your-app-token"
+    {
+     "id": 3477,
+     "name": "São Paulo",
+     "state": "SP",
+     "country": "BR",
+     "data": [
+     {
+          "date": "2019-09",
+          "climate_temperature": {
+              "last_year": {
+                  "min": 15,
+                  "max": 26
+               },
+               "normal": {
+                  "min": 14,
+                  "max": 24
+               },
+               "forecast": {
+                  "min": 17,
+                  "max": 27
+               }
+          },
+      },
+      {
+          "date": "2019-10",
+          "climate_temperature": {
+              "last_year": {
+                  "min": 17,
+                  "max": 25
+               },
+               "normal": {
+                  "min": 15,
+                  "max": 25
+               },
+               "forecast": {
+                  "min": 17,
+                  "max": 27
+               }
+          },
+      }
+      ...
+      ]
+    }
+  */
 }
---
+++ src/app/app.models.ts
@@ -0,0 +1,13 @@
+export interface Cidade {
+    id: number;
+    name: string;
+    state: string;
+    country: String;
+  }
+  
+  export interface CidadeClima {
+    id: number;
+    name: string;
+    state: string;
+    country: String;
+  }
